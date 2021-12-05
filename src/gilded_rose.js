786adaa877a75e20989c8ecf56540e8226d3910b
Your Name <you@example.com>
2021-12-05 23:14:29 +0530

    revamped code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -58,6 +58,27 @@ const update_quality = (items) => {
               : item.quality - 1
             : item.quality;
         break;
+      case book2:
+        item.sell_in = item.sell_in - 1;
+        item.quality =
+          item.quality < 50
+            ? item.sell_in < 0
+              ? item.quality + 2
+              : item.quality + 1
+            : item.quality;
+        break;
+
+      case book5:
+        item.sell_in = item.sell_in - 1;
+        item.quality =
+          item.sell_in < 0
+            ? item.quality - item.quality
+            : item.quality < 50
+            ? item.sell_in < 11 || item.sell_in < 6
+              ? item.quality + 2
+              : item.quality + 1
+            : item.quality;
+        break;
     }
   });
 };
